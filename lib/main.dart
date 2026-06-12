22305f69ee130fb34843393c24ede9dc2b366b4a
Gokul 🦈 <itsmegokulkp@gmail.com>
2026-06-12 11:11:43 +0530

    firebase completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,13 @@ import 'package:first_app/login.dart';
 import 'package:first_app/signup.dart';
 import 'package:first_app/welcome.dart';
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+void main() async{await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Welcome()));
 }
